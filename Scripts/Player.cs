ffa4c8cbb290448461c71dd1a7375039ea97a27f
Yuri Ferreira <64503593+Yuri-Ferreira-27@users.noreply.github.com>
2022-11-03 19:56:00 -0300

    Add files via upload

---
+++ Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿//Player aula 29/09
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public float velocidade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Olá, Eu sou o objeto" + gameObject.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movHorizontal = Input.GetAxis("Horizontal");
+		float movVertical = Input.GetAxis("Vertical");
+		
+		Vector3 movimento = new Vector3(movHorizontal,0,movVertical);
+		
+		gameObject.transform.GetComponent<Rigidbody>().AddForce(movimento * velocidade);
+    }
+}
